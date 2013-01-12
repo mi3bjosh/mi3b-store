5a84f7beb7ea3cc1e25ae5912b129ce25c9f4a9b
ShellaSilviaT <okyp.chei@gmail.com>
2013-01-12 11:18:33 +0700

    create models by : shella & vita
    mambuat model untuk news (admin)

--- application/models/news_model.php
+++ application/models/news_model.php
@@ -1,1 +1,63 @@
-buat shella dkk
+<?php
+class News_model extends CI_Model {
+  
+	private $tbl_news= 'news';
+	
+	function __construct(){
+		parent::__construct();
+	}
+	
+	function list_all(){
+		$this->db->order_by('id','asc');
+		return $this->db->get($tbl_news);
+	}
+	
+	function count_all(){
+		return $this->db->count_all($this->tbl_news);
+	}
+	
+	function get_paged_list($limit = 10, $offset = 0){
+		$this->db->order_by('id','asc');
+		return $this->db->get($this->tbl_news, $limit, $offset);
+	}
+	
+	function get_by_id($id){
+		$this->db->where('id', $id);
+		return $this->db->get($this->tbl_news);
+	}
+	
+	function save($news){
+		$this->db->insert($this->tbl_news, $news);
+		return $this->db->insert_id();
+	}
+	
+	function update($id, $news){
+		$this->db->where('id', $id);
+		$this->db->update($this->tbl_news, $news);
+	}
+	
+	function delete($id){
+		//$this->db->where('id', $id);
+		//$this->db->delete($this->tbl_news);
+		$this->db->query('update news set is_deleted=1 where id='.$id.'');
+	}
+	
+	function undelete($id){
+		//$this->db->where('id', $id);
+		//$this->db->delete($this->tbl_news);
+		$this->db->query('update news set is_deleted=0 where id='.$id.'');
+	}
+	
+	function hide($id, $news){
+		$this->db->where('id', $id);
+		$this->db->update($this->tbl_news, $news);
+	}
+	
+	function unhide($id, $news){
+		$this->db->where('id', $id);
+		$this->db->update($this->tbl_news, $news);
+	}
+	
+	
+}
+?>
